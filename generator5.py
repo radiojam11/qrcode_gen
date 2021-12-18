1f3d14b183b5a394bfde6553a98b4a0439c97cb7
Valerio <vtognozzi@gmail.com>
2021-12-18 10:07:38 +0100

    Create generator5.py

---
+++ generator5.py
@@ -0,0 +1,10 @@
+#
+#
+
+from MyQR import myqr
+
+myqr.run(words='https://google.com/',
+         version=1,
+         picture='bg.gif',
+         colorized=True,
+         save_name='google_myqr_dynamic.gif')
